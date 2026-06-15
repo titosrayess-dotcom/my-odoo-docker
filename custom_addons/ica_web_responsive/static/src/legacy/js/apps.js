fc3bed17ef2ef13e89459073086f15352c4104fa
titosrayess-dotcom <titosrayess@gmail.com>
2026-06-15 14:56:11 +0100

    Add files via upload

---
+++ custom_addons/ica_web_responsive/static/src/legacy/js/apps.js
@@ -0,0 +1,11 @@
+odoo.define('ica_web_responsive.apps', function (require) {
+"use strict";
+
+var Apps = require('web.Apps');
+
+Apps.include({
+    // Do nothing on update count as needactions have been removed in enterprise
+    _on_update_count: function() {},
+});
+
+});
